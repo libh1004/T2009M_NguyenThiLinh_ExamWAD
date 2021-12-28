cb15717a1f2e866c776143fc57d9e97bfb5f21a0
Nguyễn Thị Linh <libh1004@gmail.com>
2021-12-28 11:02:56 +0700

    Add project files.

---
+++ T2009M_NguyenThiLinh_ExamWAD/Data/MyDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using T2009M_NguyenThiLinh_ExamWAD.Models;
+
+namespace T2009M_NguyenThiLinh_ExamWAD.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("name=ConnectionString")
+        {
+
+        }
+        public DbSet<Exam> Exams { get; set; }
+    }
+}
---
+++ T2009M_NguyenThiLinh_ExamWAD/Models/ViewModels/ExamViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T2009M_NguyenThiLinh_ExamWAD.Models.ViewModels
+{
+    public class ExamViewModel
+    {
+        public string ExamSubject { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime ExamDate { get; set; }
+        public DateTime ExamDuration { get; set; }
+        public string ClassRoom { get; set; }
+        public string Faculty { get; set; }
+    }
+}
